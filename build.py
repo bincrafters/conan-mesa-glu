0633905ba368d8c7c5c8b1f5f67139b3e91b96fa
bincrafters-user <bincrafters@gmail.com>
2020-06-12 05:09:26 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.24.4

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
     docker_entry_script = None
     if os.getenv("CONAN_GCC_VERSIONS") == "4.9":
-        docker_entry_script = "conan install ninja/1.9.0@ --build ninja"
+        docker_entry_script = "conan install ninja/1.10.0@ --build ninja"
 
     builder = build_template_default.get_builder(docker_entry_script=docker_entry_script)
 
